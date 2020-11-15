2c715242a4affa9c2b5016ab048d10bbbe26ba03
paulina-dzinowagis <paulina01865@gmail.com>
2020-11-15 14:47:42 +0100

    switch to System.out.printf

--- src/main/java/org/example/basics/Assignments.java
+++ src/main/java/org/example/basics/Assignments.java
@@ -6,6 +6,6 @@ public class Assignments {
         int value1 = 2;
         int value2= 3;
         int value3 = 4;
-        System.out.println(String.format("%d %d %d %d", value, value1, value2, value3));
+        System.out.printf("%d %d %d %d%n", value, value1, value2, value3);
     }
 }

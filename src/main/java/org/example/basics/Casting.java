5902b9483ff752b1d0682c68fc1e04b117eb5f85
paulina-dzinowagis <paulina01865@gmail.com>
2020-11-15 19:15:41 +0100

    add Casting with explicit casting examples

---
+++ src/main/java/org/example/basics/Casting.java
@@ -0,0 +1,9 @@
+package org.example.basics;
+
+public class Casting {
+    public static void main(String[] args) {
+        float f = (float) (20d + 20.02f);
+        long n = (long) (10d + 2L);
+        byte b = (byte) (n + 5);
+    }
+}

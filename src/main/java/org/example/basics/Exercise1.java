84a58106f8fd186a686b903a7b89dc036dfabe07
paulina-dzinowagis <paulina01865@gmail.com>
2020-11-15 14:39:50 +0100

    add Exercise1 with assigned values

---
+++ src/main/java/org/example/basics/Exercise1.java
@@ -0,0 +1,11 @@
+package org.example.basics;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        int value = 1;
+        int value1 = 2;
+        int value2= 3;
+        int value3 = 4;
+        System.out.println(String.format("%d %d %d %d", value, value1, value2, value3));
+    }
+}

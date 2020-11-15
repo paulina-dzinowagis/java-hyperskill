fe9ce797d0090e40043f059592d56ad3b721da9f
paulina-dzinowagis <paulina01865@gmail.com>
2020-11-15 20:33:43 +0100

    create scanning package and create Scanning1 and Scanning2

---
+++ src/main/java/org/example/basics/scanning/Scanning1.java
@@ -0,0 +1,10 @@
+package org.example.basics.scanning;
+
+import java.util.Scanner;
+
+public class Scanning1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.next();
+    }
+}
---
+++ src/main/java/org/example/basics/scanning/Scanning2.java
@@ -0,0 +1,13 @@
+package org.example.basics.scanning;
+
+import java.util.Scanner;
+
+public class Scanning2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.next());
+        System.out.println(scanner.next());
+        System.out.println(scanner.next());
+        System.out.println(scanner.next());
+    }
+}

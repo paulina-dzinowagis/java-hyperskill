82abda6b677439173ff1bf61358e87782f1e1983
paulina-dzinowagis <paulina01865@gmail.com>
2020-11-15 20:20:12 +0100

    add Scanning waiting for one word input from user

---
+++ src/main/java/org/example/basics/Scanning.java
@@ -0,0 +1,11 @@
+package org.example.basics;
+
+import java.util.Scanner;
+
+public class Scanning {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input one word");
+        System.out.println(scanner.next());
+    }
+}

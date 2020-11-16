ce437cb5224bbc812f681317f611fabe6b9b75de
paulina-dzinowagis <paulina01865@gmail.com>
2020-11-16 22:04:10 +0100

    create new class with new task response

--- src/main/java/org/example/basics/scanning/Scanning5.java
+++ src/main/java/org/example/basics/scanning/Scanning5.java
@@ -1,4 +1,23 @@
 package org.example.basics.scanning;
 
+import java.util.Scanner;
+
 public class Scanning5 {
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            String name1 = scanner.next();
+            String name2 = scanner.next();
+            String name3 = scanner.next();
+            String name4 = scanner.next();
+            String name5 = scanner.next();
+            String name6 = scanner.next();
+            String name7 = scanner.next();
+        System.out.println(name1);
+        System.out.println(name2);
+        System.out.println(name3);
+        System.out.println(name4);
+        System.out.println(name5);
+        System.out.println(name6);
+        System.out.println(name7);
+    }
 }

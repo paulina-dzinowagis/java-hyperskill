43409be29474d4bb92da56993c17525413eda1f8
paulina-dzinowagis <paulina01865@gmail.com>
2020-11-16 21:34:09 +0100

    create Scanning5, scanner.next changed to scanner.nextLine

--- src/main/java/org/example/basics/scanning/Scanning4.java
+++ src/main/java/org/example/basics/scanning/Scanning4.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Scanning4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.next();
+        String name = scanner.nextLine();
         System.out.println("Hello" + " " + name);
     }
 }
---
+++ src/main/java/org/example/basics/scanning/Scanning5.java
@@ -0,0 +1,4 @@
+package org.example.basics.scanning;
+
+public class Scanning5 {
+}

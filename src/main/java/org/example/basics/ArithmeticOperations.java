9fbb3d1c5e18c0fd9bea5227a9595da38a3744c6
paulina-dzinowagis <paulina01865@gmail.com>
2020-11-16 22:34:44 +0100

    create ArithmeticOperations with writing complex expressions

---
+++ src/main/java/org/example/basics/ArithmeticOperations.java
@@ -0,0 +1,10 @@
+package org.example.basics;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        System.out.println(2 + 4 * 5 - 10 / (2 + 1));
+        System.out.println(-10 * (3 + 4) / 2);
+        System.out.println(2 + (-3) + 5 + (-8));
+        System.out.println((-5) * (-10) * (-20));
+    }
+}

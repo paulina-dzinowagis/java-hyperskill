cfcfb59ab74a629372505590b47b7cb21f247bfb
paulina-dzinowagis <paulina01865@gmail.com>
2020-11-16 18:18:06 +0100

    add Scanning4 waiting for name input from user

---
+++ src/main/java/org/example/basics/scanning/Scanning3.java
@@ -0,0 +1,14 @@
+package org.example.basics.scanning;
+
+import java.util.Scanner;
+
+public class Scanning3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.next());
+        System.out.println(scanner.next());
+        System.out.println(scanner.next());
+        System.out.println(scanner.next());
+    }
+}
---
+++ src/main/java/org/example/basics/scanning/Scanning4.java
@@ -0,0 +1,11 @@
+package org.example.basics.scanning;
+
+import java.util.Scanner;
+
+public class Scanning4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("Hello" + " " + name);
+    }
+}

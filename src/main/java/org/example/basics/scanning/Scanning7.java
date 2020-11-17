b108191430f69730d8cdee24ce12011766266d9b
paulina-dzinowagis <paulina01865@gmail.com>
2020-11-17 11:24:33 +0100

    create Scanning7 that reads two integer numbers from the standard input and outputs them in the reverse order separated by one space

---
+++ src/main/java/org/example/basics/scanning/Scanning7.java
@@ -0,0 +1,13 @@
+package org.example.basics.scanning;
+
+import java.util.Scanner;
+
+public class Scanning7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        System.out.print(K + " ");
+        System.out.print(N);
+    }
+}
